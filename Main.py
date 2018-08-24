55adc204fb0231aa785a1b09ba6817709e621227
UNLUCK3 <bryndan.eigl@me.com>
2018-08-24 00:04:00 -0700

    V1.0 Stable, won't spam, and wont exit randomly. Code needs refactoring.

--- Main.py
+++ Main.py
@@ -1,7 +1,15 @@
 import praw
 import IDandSecret
-from praw.models import MoreComments
 import time
+import os
+
+if not os.path.isfile("posts_replied_to.txt"):
+    posts_replied_to = []
+else:
+    with open("posts_replied_to.txt", "r") as f:
+       posts_replied_to = f.read()
+       posts_replied_to = posts_replied_to.split("\n")
+       posts_replied_to = list(filter(None, posts_replied_to))
 
 identity = IDandSecret.ClientID
 secretpass = IDandSecret.ClientSecret
@@ -9,11 +17,6 @@ botpassword = IDandSecret.MyPass
 usernames = ['/u/UNLUCK3', '/u/DiscreetBot']
 reply_text = 'Hey ', usernames, 'This guy got a knife inside Canada somehow! Do you want it?     ^^I\'m ^^a ^^bot.    ' \
                                 '  ^^PM ^^/u/UNLUCK3 ^^for ^^help, ^^or ^^to ^^add ^^yourself '
-time.sleep(500)  # for testing
-
-def replymachine(text):
-    if text == 'reply':
-        submission.reply(reply_text)
 
 
 reddit = praw.Reddit(user_agent='CanadaKnifeBot (by /u/UNLUCK3', client_id=identity, client_secret=secretpass,
@@ -21,28 +24,39 @@ reddit = praw.Reddit(user_agent='CanadaKnifeBot (by /u/UNLUCK3', client_id=ident
 
 subreddit = reddit.subreddit('DiscreetTest')
 
-for submission in subreddit.stream.submissions():
+def mainloop():
+    counter1 = 0
+    for submission in subreddit.new(limit=5):
+        counter1 = counter1 + 1
+        if submission.id not in posts_replied_to: # what I want is for it to print the as they come in while running,
+            # idk how to do that though...
+            print("These posts are new: \n")
+            print(counter1)
+            print("Title: ", submission.title)
+            print("Text: ", submission.selftext, "\n")
 
-    callings = ['canada', 'canadian', '🇨🇦']
-    normalized_title = submission.title.lower()
-    normalized_text = submission.selftext.lower()
-    alreadydone = 0
+        callings = ['canada', 'canadian', '🇨🇦']
+        normalized_title = submission.title.lower()
+        normalized_text = submission.selftext.lower()
 
-    for top_level_comment in submission.comments:
-        if isinstance(top_level_comment, MoreComments):
-            continue
-        if top_level_comment.body == reply_text:
-            alreadydone = 1
-        else:
-            alreadydone = 0
-        if alreadydone == 1:
-            break
-        elif alreadydone == 0:
-            for canadian_mentions in callings:
-                if canadian_mentions in normalized_title:
-                    replymachine('reply')
-                    break
-                elif canadian_mentions in normalized_text:
-                    replymachine('reply')
-                    break
-            time.sleep(300)
+        while counter1 <= 99:
+            time.sleep(30) # change to a higher number when not testing
+            if submission.id not in posts_replied_to:
+                for canadian_mentions in callings:
+                    if canadian_mentions in normalized_title:
+                        submission.reply(reply_text)
+                        print("Bot replying to : ", submission.title)
+                        posts_replied_to.append(submission.id)
+                    elif canadian_mentions in normalized_text:
+                        submission.reply(reply_text)
+                        print("Bot replying to : ", submission.selftext)
+                        posts_replied_to.append(submission.id)
+                    else:
+                        print("No applicable posts right now.")
+                    with open("posts_replied_to.txt", "w") as f:
+                        for post_id in posts_replied_to:
+                            f.write(post_id + "\n")
+            mainloop()
+
+mainloop()
+
