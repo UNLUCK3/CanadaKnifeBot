134da46e9c7565c52a5fc392cb274d16d6b89486
UNLUCK3 <bryndan.eigl@me.com>
2018-08-23 19:49:16 -0700

    updating

--- Main.py
+++ Main.py
@@ -1,1 +1,4 @@
-placeholder
+import praw
+import IDandSecret
+
+reddit = praw.Reddit(user_agent='CanadaKnifeBot (by /u/UNLUCK3', client_ID=IDandSecret.clientID, client_secret=IDandSecret.ClientSecret, username='UNLUCK3', password=IDandSecret.MyPass)
